b19f401d803937eadc349bcae8ca0ecf72477521
Joe Seiden <joe.seiden214@gmail.com>
2017-08-28 22:29:15 -0700

    Add validation for word generator input

--- word_generator.rb
+++ word_generator.rb
@@ -3,8 +3,7 @@ require_relative "word_generator"
 class WordGenerator
 
   def initialize(consonants, vowels, syllable_structure, length)
-    # fill in validation method to raise errors if any are bad
-    validate_initial_variables
+    validate_initial_variables(consonants, vowels, syllable_structure, length)
     @consonants = consonants
     @vowels = vowels
     @syllable_structure = syllable_structure
@@ -31,7 +30,22 @@ class WordGenerator
 
   private
 
-  def validate_initial_variables
+  def validate_initial_variables(consonants, vowels, syllable_structure, length)
+    validate_consonants_and_vowels(consonants, vowels)
+    validate_syllable_structure(syllable_structure)
+    raise "Length must be a non-zero integer" unless length.is_a?(Integer) && length > 0
+  end
 
+  def validate_consonants_and_vowels(consonants, vowels)
+    raise "Consonants must be an array of single character strings" unless array_of_single_char_strings?(consonants)
+    raise "Vowels must be an array of single character strings" unless array_of_single_char_strings?(vowels)
+  end
+
+  def array_of_single_char_strings?(array)
+    array.is_a?(Array) && array.all? { |e| e.is_a?(String) && e.length == 1 }
+  end
+
+  def validate_syllable_structure(structure)
+    raise "Structure is either not a string or is not in the correct format" unless structure.is_a?(String) && structure =~ /^[C]*[V]+[C]*$/
   end
 end
